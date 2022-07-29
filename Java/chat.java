68a9d12822b338180ba4368f66337eab780b6a34
Stella <stellavir11@gmail.com>
2022-07-29 16:07:57 +0300

    Add files via upload

--- Java/chat.java
+++ Java/chat.java
@@ -155,6 +155,49 @@ Thread sreceive;
 	ssend.join():
 	sreceive.join();
 
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
+
+public class UnSyncChatClient {
+	public static void main(String args[]) throws IOException, InterruptedException {
+		DatagramSocket cs = new DatagramSocket(5334);
+		InetAddress ip = InetAddress.getLocalHost();
+	
+	System.out.println("Client is up.");
+
+	Thread csend;
+	public void run() {
+	Scanner sc = new Scanner(System.in);
+	while(true) {
+		synchronized(this) {
+		byte[] sd = new byte[1000];
+		sd = sc.nextLine().getBytes();
+		DatagramPacket sp = new DatagramPacket(sd, sd.length, ip, 1234);
+		cs.send(sp);
+		String msg = new String(sd);
+		
+		}
+	Thread creceive;
+	creceive = new Thread(new Runnable() {
+	@Override 
+	public void run() {
+		while (true)
+		synchronized (this) {
+			byte[] rd = new byte(1000);
+		DatagramPacket sp1 = new DatagramPacket(rd, rd.length);
+	cs.receive(sp1);
+	String msg = (new String(rd)).trim():
+	
+	csend.start():
+	creceive.start():
+	
+	csend.join();
+	creceive.join();
+	}
+   }
+
+
 // end of the file eof
 
 
